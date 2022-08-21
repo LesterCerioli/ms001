6dea3f894f06cb0578c63972d26a1261a3c6661c
lester <lester.cerioli@lucastechnologyservice.com>
2022-08-21 18:20:59 -0300

    Building Logs API structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogsModule } from './logs/logs.module';
+import { LogsModule } from './logs/logs.module';
 
 @Module({
   imports: [LogsModule],
